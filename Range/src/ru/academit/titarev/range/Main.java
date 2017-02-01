938592e86f7cd2d1a7ce1d215c87ef8acd848a8b
TitarevVsevolod <sevich1@mail.ru>
2017-02-01 23:10:49 +0700

    Range (simple version) correction

---
+++ Range/src/ru/academit/titarev/range/Main.java
@@ -0,0 +1,11 @@
+package ru.academit.titarev.range;
+
+public class Main {
+    public static void main(String[] args) {
+        Range range = new Range(5, 18.7);
+        double lengthOfInterval = range.findLength();
+        boolean inside = range.isInside(-15);
+        System.out.println(lengthOfInterval);
+        System.out.println(inside);
+    }
+}
--- Range/src/ru/academit/titarev/range/Range.java
+++ Range/src/ru/academit/titarev/range/Range.java
@@ -9,29 +9,14 @@ public class Range {
         this.to = to;
     }
 
-    public double findLengthOfInterval() {
-        return Math.abs(to - from);
+    public double findLength() {
+        return to - from;
     }
 
     public boolean isInside(double x) {
-        if (from > to) {
-            if (x >= to && x <= from) {
-                return true;
-            }
-        } else {
-            if (x >= from && x <= from) {
-                return true;
-            }
+        if ((x >= from) && (x <= to)) {
+            return true;
         }
         return false;
-    }
-
-    public static void main(String[] args) {
-        Range range = new Range(-5, -18.7);
-        double lengthOfInterval = range.findLengthOfInterval();
-        boolean inside = range.isInside(-20);
-        System.out.println(lengthOfInterval);
-        System.out.println(inside);
-
     }
 }
