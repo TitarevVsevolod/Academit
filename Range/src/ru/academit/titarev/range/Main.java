32c775c58c15a9094aff01cde05612471e23c6ab
TitarevVsevolod <sevich1@mail.ru>
2017-02-08 01:21:24 +0700

    Range (difficult version) Сделал часть с нахождением пересечения.

--- Range/src/ru/academit/titarev/range/Main.java
+++ Range/src/ru/academit/titarev/range/Main.java
@@ -1,11 +1,18 @@
 package ru.academit.titarev.range;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Range range = new Range(5, 18.7);
-        double lengthOfInterval = range.getLength();
-        boolean inside = range.isInside(-15);
+        Range range1 = new Range(0, 4);
+        Range range2 = new Range(5, 6);
+        double lengthOfInterval = range1.getLength();
+        boolean inside = range1.isInside(-15);
+        Range rangeCrossing = range1.getCrossing(range2);
+        String rangeAssociation = range1.getAssociation(range2);
         System.out.println(lengthOfInterval);
         System.out.println(inside);
+        System.out.println("Интервал пересечения: " + rangeCrossing);
+        System.out.println("Интервал объединения: " + (rangeAssociation));
     }
 }
--- Range/src/ru/academit/titarev/range/Range.java
+++ Range/src/ru/academit/titarev/range/Range.java
@@ -24,4 +24,52 @@ public class Range {
     public boolean isInside(double x) {
         return (x >= from) && (x <= to);
     }
+
+    public String toString() {
+        return "(" + this.from + ";" + this.to + ")";
+    }
+
+    public Range getCrossing(Range range) {
+        if (this.from < range.from && this.to < range.from) {
+            return null;
+        } else if (range.from < this.from && range.to < this.from) {
+            return null;
+        } else if (this.from < range.from && this.to < range.to) {
+            range.to = this.to;
+            return range;
+        } else if (this.from < range.from && range.to < this.to) {
+            range.from = this.from;
+            range.to = this.to;
+            return range;
+        } else if (range.from < this.from && range.to < this.to) {
+            range.from = this.from;
+            return range;
+        } else {
+            return range;
+        }
+    }
+
+    public Range[] getAssociation(Range range) {
+        Range[] arr = new Range[2];
+        if (this.from < range.from && this.to < range.from) {
+
+            arr[0] = range;
+            arr[1] = this;
+            return arr;
+        } /*else if (range.from < this.from && range.to < this.from) {
+            return null;
+        } else if (this.from < range.from && this.to < range.to) {
+            range.from = this.from;
+            return range;
+        } else if (this.from < range.from && range.to < this.to) {
+            range.from = this.from;
+            range.to = this.to;
+            return range;
+        } else if (range.from < this.from && range.to < this.to) {
+            range.to = this.to;
+            return range;
+        } */else {
+            return null;
+        }
+    }
 }
